47d274bbc620237b88402fd98d165b8a59ab24f5
JoseR4Geeks <143768985+JoseR4Geeks@users.noreply.github.com>
2023-11-02 00:27:02 +0000

    added photos to cards

--- src/js/component/Card/card.jsx
+++ src/js/component/Card/card.jsx
@@ -6,7 +6,7 @@ const Card = () => {
       <div className="row">
         <div className="col-md-3">
           <div className="card" style={{ width: "18rem" }}>
-            <img className="card-img-top" src="..." alt="Card image cap" />
+            <img className="card-img-top" src="https://picsum.photos/100/75" alt="Card image cap" />
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -17,7 +17,7 @@ const Card = () => {
         
         <div className="col-md-3">
           <div className="card" style={{ width: "18rem" }}>
-            <img className="card-img-top" src="..." alt="Card image cap" />
+            <img className="card-img-top" src="https://picsum.photos/100/75" alt="Card image cap" />
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -28,7 +28,7 @@ const Card = () => {
         
         <div className="col-md-3">
           <div className="card" style={{ width: "18rem" }}>
-            <img className="card-img-top" src="..." alt="Card image cap" />
+            <img className="card-img-top" src="https://picsum.photos/100/75" alt="Card image cap" />
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -39,7 +39,7 @@ const Card = () => {
 
         <div className="col-md-3">
           <div className="card" style={{ width: "18rem" }}>
-            <img className="card-img-top" src="..." alt="Card image cap" />
+            <img className="card-img-top" src="https://picsum.photos/100/75" alt="Card image cap" />
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
